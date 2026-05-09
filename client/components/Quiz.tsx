400d26a329def4faf22aa2652aa405df2a37cf03
TechAsish1212 <gitasish1212@gmail.com>
2026-05-09 19:40:14 +0530

    feat : add question page

--- client/components/Quiz.tsx
+++ client/components/Quiz.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import QuizStart from './QuizStart'
+import ProgressBar from './ProgressBar';
+import Timer from './Timer';
+import Question from './Question';
 
 const Quiz = () => {
   return (
-    <QuizStart/>
+    // <QuizStart/>
+    // <ProgressBar/>
+    // <Timer/>\
+    <Question/>
     // <div className='min-h-screen bg-gradient-to-br from-indigo-100 to-purple-100 py-8 px-4'>
     //     <div className='max-w-4xl mx-auto mb-8'>
     //         <div className='bg-white p-6 shadow-lg rounded-xl'>
